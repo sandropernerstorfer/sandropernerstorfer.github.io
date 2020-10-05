1c48872fc0b2f154a455fa3fa2a074850817f628
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-10-06 00:29:21 +0200

    language.js: content added

---
+++ script/languages.js
@@ -0,0 +1,17 @@
+const english = 
+    {
+        modal : "Choose Language",
+        desc : "Web Development",
+        nav1 : "About Me",
+        nav2 : "Projects",
+        nav3 : "Contact"
+    };
+
+const german = 
+    {
+        modal : "Sprache ändern",
+        desc : "Web Entwicklung",
+        nav1 : "Über Mich",
+        nav2 : "Projekte",
+        nav3 : "Kontakt"
+    };
