853fa1304b273ecd889ddbbb2bc91fbf4cb49e35
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-10-09 12:57:46 +0200

    language.js file expanded

--- script/languages.js
+++ script/languages.js
@@ -2,13 +2,15 @@ const english =
     {
         modal : "Choose Language",
         desc : "Web Development",
-        nav1 : "About Me",
+        nav1 : "About",
         nav2 : "Projects",
         nav3 : "Contact",
-        about : "About Me",
+        about : "About",
         greet : "Hey i´m Sandro !",
         ab1 : "A motivated Web-Developer from Vienna with a desire to learn.",
-        ab2 : "I created this Page to offer an overview of my Work and myself."
+        ab2 : "I created this Page to offer an overview of my Work and myself.",
+        skill1 : "I mainly work with :",
+        skill2 : "...and have experience with :"
     };
 
 const german = 
@@ -21,5 +23,7 @@ const german =
         about : "Über Mich",
         greet : "Hey ich bin Sandro !",
         ab1 : "Ein motivierter und lerneifriger Web-Entwickler aus Wien.",
-        ab2 : "Ich habe diese Seite erstellt um einen Überblick von mir und meiner Arbeit zu schaffen."
+        ab2 : "Ich habe diese Seite erstellt um einen Überblick von mir und meiner Arbeit zu schaffen.",
+        skill1 : "Ich arbeite hauptsächlich mit :",
+        skill2 : "...und habe Erfahrung mit :"
     };
