fb1263e709429931c5ce0ac9e15b0760ad455629
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-10-11 23:34:35 +0200

    added more text for about section

--- script/languages.js
+++ script/languages.js
@@ -8,7 +8,8 @@ const english =
         about : "About",
         greet : "Hey i´m Sandro !",
         ab1 : "A motivated Web-Developer from Vienna with a desire to learn.",
-        ab2 : "I created this Page to offer an overview of my Work and myself.",
+        ab2 : "I always had a good time playing around with creativity and logic, so some of my biggest interests are: Tech, Development, Art and playing Guitar.",
+        ab3 : "This page was created to offer a quick overview of my work, and myself.",
         skill1 : "I mainly work with :",
         skill2 : "...and have experience with :"
     };
@@ -23,7 +24,8 @@ const german =
         about : "Über Mich",
         greet : "Hey ich bin Sandro !",
         ab1 : "Ein motivierter und lerneifriger Web-Entwickler aus Wien.",
-        ab2 : "Ich habe diese Seite erstellt um einen Überblick von mir und meiner Arbeit zu schaffen.",
+        ab2 : "Ich hatte immer schon sehr viel Spass an dem Zusammenspiel von Kreativität und Logik, daher zählen zu meinen größten Interessen: Technik, Entwicklung, Kunst und Gitarrespielen.",
+        ab3 : "Diese Seite wurde erstellt um einen kleinen Überblick von mir und ein paar meiner Projekte zu schaffen.",
         skill1 : "Ich arbeite hauptsächlich mit :",
         skill2 : "...und habe Erfahrung mit :"
     };
