6ef4a48038cc74377600f24564fd8ed47abf70cd
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-11-11 23:47:48 +0100

    language object updated

--- script/languages.js
+++ script/languages.js
@@ -6,12 +6,12 @@ const english =
         modal : "Choose Language",
         desc : "Web Developer",
         about : "About",
-        greet : "Hey, i´m Sandro,",
+        greet : "I am Sandro Pernerstorfer,",
         ab1 : "a motivated Web-Developer from Vienna with a desire to learn.",
-        ab2 : "I always had a good time playing around with creativity and logic, so some of my biggest interests are: Tech, Development, Art and playing Guitar.",
+        ab2 : "Combining creativity and logic to develop great products has always inspired me. So i am constantly learning and improving myself to help and make the Web a beautiful place.",
         ab3 : "This page was created to offer a quick overview of my work, and myself.",
         skill1 : "I mainly work with",
-        skill2 : "...and have experience with"
+        skill2 : "and have experience with"
     };
 
 const german = 
@@ -22,10 +22,10 @@ const german =
         modal : "Sprache ändern",
         desc : "Web Entwickler",
         about : "Über Mich",
-        greet : "Hey, ich bin Sandro,",
+        greet : "Ich bin Sandro Pernerstorfer,",
         ab1 : "ein motivierter und lerneifriger Web-Entwickler aus Wien.",
-        ab2 : "Ich hatte immer schon sehr viel Spass an dem Zusammenspiel von Kreativität und Logik, daher zählen zu meinen größten Interessen: Technik, Entwicklung, Kunst und Gitarrespielen.",
+        ab2 : "Kreativität und Logik zu kombinieren um großartige Produkte zu erschaffen hat mich schon immer inspiriert und dazu bewegt, stets neues zu lernen.",
         ab3 : "Diese Seite wurde erstellt um einen kleinen Überblick von mir und ein paar meiner Projekte zu schaffen.",
         skill1 : "Ich arbeite hauptsächlich mit",
-        skill2 : "...und habe Erfahrung mit"
+        skill2 : "und habe Erfahrung mit"
     };
