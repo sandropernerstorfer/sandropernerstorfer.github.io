47796514a66f4e2e29033db9c56b879b890f5d21
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2020-11-22 00:22:25 +0100

    project data added in projects.js

---
+++ script/projects.js
@@ -0,0 +1,31 @@
+const project1 = 
+    {
+        name : "",
+        techs : "-",
+        desc : "Work for Project 1 in progress",
+        link : ""
+    };
+
+const project2 = 
+    {
+        name : "",
+        techs : "-",
+        desc : "Work for Project 2 in progress",
+        link : ""
+    };
+
+const project3 = 
+    {
+        name : "",
+        techs : "-",
+        desc : "Work for Project 3 in progress",
+        link : ""
+    };
+
+const project4 = 
+    {
+        name : "",
+        techs : "-",
+        desc : "Work for Project 4 in progress",
+        link : ""
+    };
