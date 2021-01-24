7bda619589d53181bc692a68f88deb14da200f05
Sandro Pernerstorfer <sandro.pernerstorfer@hotmail.com>
2021-01-24 15:59:02 +0100

    new project added

--- script/projects.js
+++ script/projects.js
@@ -1,19 +1,19 @@
 const project1 = 
     {
+        name : "Express User-Manager",
+        techs : "NodeJS, ExpressJS",
+        desc : "Small User-Management System with validation, search-, and sorting-functions. Built with Node and Express.",
+        githubLink : "https://github.com/sandropernerstorfer/Node-Express-UserManager",
+        liveLink : "https://node-express-user-manager.herokuapp.com/"
+    };
+
+const project2 = 
+    {
         name : "Ghostwriter",
         techs : "HTML, SCSS, JS",
         desc : "Ghostwriter is a Frontend Tool that will take two user inputs : Text and Milliseconds. In the Preview Window you will then see your text displayed letter after letter for the wanted duration. Below you can copy the already generated code with one click to use wherever you want.",
         githubLink : "https://github.com/sandropernerstorfer/ghostwriter-app/tree/master",
         liveLink : "https://ghostwriter-js.tech/"
-    };
-
-const project2 = 
-    {
-        name : "",
-        techs : "-",
-        desc : "Work for Project 2 in progress",
-        githubLink : "",
-        liveLink : ""
     };
 
 const project3 = 
